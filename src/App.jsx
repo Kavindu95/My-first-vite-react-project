5078eee0b4b62df4834d201f556ec25c0d82de25
Kavindu95 <kavindueng95@gmail.com>
2022-11-17 19:31:14 +0530

    project added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Main from './Component/Main'
+import Navbar from './Component/Navbar'
+import './App.css'
+
+export default function App() {
+ 
+  return (
+    <div className="App">
+      <Navbar/>
+      <Main/>
+
+    </div>
+  )
+}
+
+
